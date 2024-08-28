208001bb8795bb06f7a8a153f5642c0cf051a468
greenhorn990628 <greenhorn990628@163.com>
2024-08-28 11:40:55 +0800

    增加取消快速会议接口
    增加取消快速会议接口，通过会议id来判断是否可以删除以及根据会议id删除会议

---
+++ web/appapi/cancel_fast_meeting.php
@@ -0,0 +1,35 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace MRBS;
+require_once "../mrbs_sql.inc";
+require "../defaultincludes.inc";
+require_once "api_helper.php";
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+$meeting_id = $data["meeting_id"];
+
+$meeting = DBHelper::one(_tbl("entry"), "id = $meeting_id");
+
+if (empty($meeting)) {
+  ApiHelper::fail("meeting not found", -1);
+  return;
+}
+
+if ($meeting["name"] != "Fast Meeting"){
+  ApiHelper::fail("only fast meeting can be canceled", -2);
+}
+
+if ($meeting["end_time"] < time()){
+  ApiHelper::fail("meeting has expired", -3);
+}
+
+$result = db() -> query("DELETE FROM " . _tbl("entry") . " WHERE id = ?", array($meeting_id));
+if ($result) {
+  ApiHelper::success(null);
+}else{
+  ApiHelper::fail("Mysql error", -4);
+}
