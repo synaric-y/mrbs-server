8661a62f29224e41ed5c2116eceaa577d96a75c1
greenhorn990628 <greenhorn990628@163.com>
2024-09-09 14:30:50 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -20,6 +20,7 @@ $type = $data['type'];
 $id = $data['id'];
 $start_time = $data['start_time'];
 $end_time = $data['end_time'];
+$timezone = $data['timezone'];
 $response = array(
   "code" => 'int',
   "message" => 'string'
@@ -95,7 +96,7 @@ $rows = $result -> all_rows_keyed();
 $default_timezone = date_default_timezone_get();
 foreach ($rows as $row) {
   if (!empty($row['timezone']))
-    date_default_timezone_set($row['timezone']);
+    date_default_timezone_set($timezone);
   else
     date_default_timezone_set($default_timezone);
   $areaId = $row['area_id'];
