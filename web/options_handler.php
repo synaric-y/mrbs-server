94ad74f48f5d434a9b984c5b162781648cc7f376
greenhorn990628 <greenhorn990628@163.com>
2024-09-18 17:52:36 +0800

    拒绝所有除企微登录功能以外的get请求，并以此为入口

--- web/options_handler.php
+++ web/options_handler.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * 全局入口函数，用于处理OPTIONS请求以及拒绝所有非POST请求
+ * 对于所有的POST请求，将所有的POST请求体解析至超全局参数$_POST中
+ */
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 //  header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
@@ -7,5 +12,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   header('Access-Control-Max-Age: 86400');
   header('Access-Control-Allow-Credentials: true');
   http_response_code(200);
-  exit();
+  exit;
 }
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' && $_GET["act"] != 'wxwork_login') {
+  // 设置 HTTP 状态码为 405 Method Not Allowed
+  header('HTTP/1.1 405 Method Not Allowed');
+  // 指定允许的请求方法
+  header('Allow: POST');
+  // 终止脚本执行
+  exit;
+}
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+$_POST = $data;
