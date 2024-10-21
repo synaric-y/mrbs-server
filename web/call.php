b6c8d6613cc4de42225a21257a03a1608c8eb375
synaric.yong <synaric.yong@bcc.global>
2024-10-21 14:55:37 +0800

    修复用户组的删除问题

--- web/call.php
+++ web/call.php
@@ -24,9 +24,9 @@ header("Pragma: no-cache");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTION');
-//ini_set('display_errors', 1);            //错误信息
-//ini_set('display_startup_errors', 1);    //php启动错误信息
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);            //错误信息
+ini_set('display_startup_errors', 1);    //php启动错误信息
+error_reporting(E_ALL);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 include_once "defaultincludes.inc";
