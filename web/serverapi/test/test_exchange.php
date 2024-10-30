6bf3eda6e07e8f3726c90937ce7fe6648df3d1fe
greenhorn990628 <greenhorn990628@163.com>
2024-10-30 16:46:35 +0800

    根据输入判断是否可以连接Exchange以及账号密码

--- web/serverapi/test/test_exchange.php
+++ web/serverapi/test/test_exchange.php
@@ -3,6 +3,7 @@
 global $exchange_server;
 
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 2) . "/defaultincludes.inc";
 
 use garethp\ews\API;
 use garethp\ews\API\Message\SyncFolderItemsResponseMessageType;
@@ -18,14 +19,18 @@ use MRBS\ApiHelper;
  * though getting calendar item to check if the exchange server can be connected
  */
 
+$server_address = $_POST['server_address'] ?? $exchange_server;
+$username = $_POST['username'] ?? null;
+$password = $_POST['password'] ?? null;
+
 try {
 //    $api = API::withUsernameAndPassword($exchange_server, "Room.Dev3", "JI%oSlfOS9");
-  $api = API::withUsernameAndPassword($exchange_server, "123", "123");
+  $api = API::withUsernameAndPassword($server_address, $username, $password);
   $calendar = $api->getCalendar();
   $calendar->getCalendarItems();
   ApiHelper::success(null);
-} catch (GuzzleHttp\Exception\ConnectException|SoapFault $e) {
+} catch (GuzzleHttp\Exception\ConnectException|SoapFault|InvalidArgumentException $e) {
   ApiHelper::fail(get_vocab("exchange_disconnect"), ApiHelper::UNKNOWN_ERROR);
 } catch(API\Exception\UnauthorizedException $e){
-  ApiHelper::success(null);
+  ApiHelper::fail(get_vocab("invalid_username_or_password"), ApiHelper::INVALID_USERNAME_OR_PASSWORD);
 }
