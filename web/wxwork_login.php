4cb4622a107a4f1ab8a4d8a064a9b042957bdee6
synaric.yong <synaric.yong@bcc.global>
2024-11-05 09:27:39 +0800

    增加企业微信获取用户信息的重试次数

--- web/wxwork_login.php
+++ web/wxwork_login.php
@@ -14,7 +14,6 @@ require_once "./lib/Wxwork/api/src/CorpAPI.class.php";
 require_once "./lib/Wxwork/api/src/Utils.php";
 
 
-
 /*
  * 企业微信登录接口，通过企业微信获取登陆人（GET），通常通过email字段进行比对，如果email字段没有比对成功
  * 则会新创建一个用户
@@ -36,8 +35,8 @@ $config = DBHelper::one(_tbl("system_variable"), "1=1");
 
 $corpid = $config['corpid'];
 $secret = $config['secret'];
-if (isset($_SESSION) && !empty($_SESSION['user'])){
-  log_wxwork( \MRBS\get_vocab('already_login'));
+if (isset($_SESSION) && !empty($_SESSION['user'])) {
+  log_wxwork(\MRBS\get_vocab('already_login'));
   \MRBS\ApiHelper::success(\MRBS\get_vocab('already_login'));
 }
 
@@ -47,7 +46,7 @@ if (!isset($_GET) || empty($_GET["code"])) {
 }
 
 $retry = 0;
-while ($retry < 1){
+while ($retry < 2) {
   $access_token = get_access_token($corpid, $secret);
   $code = $_GET['code'];
 
@@ -61,33 +60,36 @@ while ($retry < 1){
     ApiHelper::fail("wxwork errcode: $json", ApiHelper::INTERNAL_ERROR);
 
     $file = fopen("./lib/Wxwork/api/src/mutex_lock.lock", "w+");
-    if (flock($file, LOCK_EX | LOCK_NB)){
-      try{
+    if (flock($file, LOCK_EX | LOCK_NB)) {
+      try {
         $result = refresh_access_token($corpid, $secret);
-        if($result === false || $result === -1) {
+        if ($result === false || $result === -1) {
           log_wxwork("net error or redis error");
           ApiHelper::fail("net error or redis error", ApiHelper::INTERNAL_ERROR);
-        }else if ($result != 0){
+        } else if ($result != 0) {
           log_wxwork("refresh_access_token: {$result}\n", 3, dirname(__DIR__));
           ApiHelper::fail("wxwork errcode: {$result}", ApiHelper::INTERNAL_ERROR);
         }
-      }catch (\Exception $e){
+      } catch (\Exception $e) {
+        log_wxwork("refresh access_token failed");
+        log_wxwork($e->getMessage());
+        log_wxwork($e->getTraceAsString());
         throw new Exception("internal error");
       } finally {
         flock($file, LOCK_UN);
         fclose($file);
       }
-    }else{
-      if (flock($file, LOCK_EX)){
-        try{
+    } else {
+      if (flock($file, LOCK_EX)) {
+        try {
           $access_token = get_access_token($corpid, $secret);
-        }finally{
+        } finally {
           flock($file, LOCK_UN);
           fclose($file);
         }
       }
     }
-  }else{
+  } else {
     break;
   }
   $retry++;
@@ -100,39 +102,39 @@ $data = array(
 
 $retry = 0;
 
-while($retry < 2){
+while ($retry < 2) {
   $url = HttpUtils::MakeUrl("/cgi-bin/auth/getuserdetail?access_token={$access_token}");
   $json = HttpUtils::httpPost($url, json_encode($data));
   /** @noinspection PhpStrictTypeCheckingInspection */
   $data = json_decode($json, true);
   log_wxwork("resp: $json");
   if ($data['errcode'] != 0) {
-    if ($retry != 0){
+    if ($retry != 0) {
       log_wxwork("wxwork errcode: {$data['errcode']}");
       ApiHelper::fail("wxwork errcode: {$data['errcode']}", ApiHelper::UNKNOWN_ERROR);
     }
     $file = fopen("./lib/Wxwork/api/src/mutex_lock.lock", "w+");
-    if (flock($file, LOCK_EX)){
-      try{
+    if (flock($file, LOCK_EX)) {
+      try {
         $result = refresh_access_token($corpid, $secret);
-        if($result === false || $result === -1) throw new Exception("internal error");
-        if ($result === 40029)  ApiHelper::fail(\MRBS\get_vocab("invalid_code"), ApiHelper::INVALID_CODE);
-        if ($result === 40001)  ApiHelper::fail(\MRBS\get_vocab("invalid_secret"), ApiHelper::INVALID_SECRET);
-        if ($result === 40013)  ApiHelper::fail(\MRBS\get_vocab("invalid_corpid"), ApiHelper::INVALID_CORPID);
-      }catch (\Exception $e){
+        if ($result === false || $result === -1) throw new Exception("internal error");
+        if ($result === 40029) ApiHelper::fail(\MRBS\get_vocab("invalid_code"), ApiHelper::INVALID_CODE);
+        if ($result === 40001) ApiHelper::fail(\MRBS\get_vocab("invalid_secret"), ApiHelper::INVALID_SECRET);
+        if ($result === 40013) ApiHelper::fail(\MRBS\get_vocab("invalid_corpid"), ApiHelper::INVALID_CORPID);
+      } catch (\Exception $e) {
         throw new Exception("internal error");
       } finally {
         flock($file, LOCK_UN);
         fclose($file);
       }
     }
-  }else{
+  } else {
     break;
   }
   $retry++;
 }
 
-if ($retry == 2){
+if ($retry == 2) {
   ApiHelper::fail(\MRBS\get_vocab("unknown_error"), ApiHelper::UNKNOWN_ERROR);
 }
 
@@ -149,27 +151,27 @@ if (!empty($data['userid'])) {
 try {
   $result = \MRBS\db()->query($sql);
 } catch (\Exception $e) {
-  log_wxwork($e->getMessage() ."\n". $e->getTraceAsString());
+  log_wxwork($e->getMessage() . "\n" . $e->getTraceAsString());
 }
-if ($result -> count() < 1){
+if ($result->count() < 1) {
   log_wxwork("count < 1");
 
   \MRBS\db()->begin();
-  try{
-    $transaction_ok = \MRBS\db()-> query("INSERT INTO " . \MRBS\_tbl("users") . "(level, name, display_name, password_hash, email, timestamp) VALUES (?, ?, ?, ?, ?, ?)", array(1, explode("@", $data['userid'])[0], str_replace(".", " ", explode("@", $data['userid'])[0]), $default_password_hash, $data['email'] ?? $data['userid'], date("Y-m-d H:i:s", time())));
-    if ($transaction_ok){
+  try {
+    $transaction_ok = \MRBS\db()->query("INSERT INTO " . \MRBS\_tbl("users") . "(level, name, display_name, password_hash, email, timestamp) VALUES (?, ?, ?, ?, ?, ?)", array(1, explode("@", $data['userid'])[0], str_replace(".", " ", explode("@", $data['userid'])[0]), $default_password_hash, $data['email'] ?? $data['userid'], date("Y-m-d H:i:s", time())));
+    if ($transaction_ok) {
       \MRBS\db()->commit();
       $_SESSION['user'] = explode("@", $data['userid'])[0];
-    }else{
+    } else {
       \MRBS\db()->rollback();
     }
-  }catch (\Exception $e){
+  } catch (\Exception $e) {
     \MRBS\db()->rollback();
     log_wxwork("insert user transaction failed");
     log_wxwork($e->getMessage() . $e->getTraceAsString());
     ApiHelper::fail(\MRBS\get_vocab("fail_to_create_user"), ApiHelper::FAIL_TO_CREATE_USER);
   }
-}else{
+} else {
   $row = $result->next_row_keyed();
   $_SESSION['user'] = $row['name'];
   setcookie("session_id", session_id(), time() + 24 * 60 * 60, "/web/", "", false, true);
