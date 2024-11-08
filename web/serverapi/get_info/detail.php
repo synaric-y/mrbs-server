66f6f8ee9043a647539f14a2cf5d9afd60f4d382
synaric.yong <synaric.yong@bcc.global>
2024-11-08 15:40:15 +0800

    区域详情新增绑定用户组信息

--- web/serverapi/get_info/detail.php
+++ web/serverapi/get_info/detail.php
@@ -25,20 +25,32 @@ if (getLevel($_SESSION['user']) < 2){
   ApiHelper::fail(get_vocab("no_right"), ApiHelper::ACCESS_DENIED);
 }
 
+$count = 0;
+$back = null;
 if ($type == 'area'){
   $result = db() -> query("SELECT * FROM " . _tbl("area") . " WHERE id = ?", array($id));
+  $count = $result->count();
+  $back = $result->all_rows_keyed();
+  $sql = "SELECT G.id, G.name, G.source FROM " . _tbl("a2g_map") . " A2G LEFT JOIN " . _tbl("user_group") . " G ON" .
+    " A2G.group_id = G.id WHERE A2G.area_id = ? AND group_id != -1";
+  $groups = db() -> query($sql, [$id])->next_row_keyed();
+  if (!empty($back[0]) && !empty($groups)) {
+    $back[0]['groups'] = $groups;
+  }
 }else if ($type == 'room'){
   $result = db() -> query("SELECT * FROM " . _tbl("room") . " WHERE id = ?", array($id));
+  $count = $result->count();
+  $back = $result->all_rows_keyed();
 }else if ($type == 'device'){
 
 }else{
   ApiHelper::fail(get_vocab("wrong_type"), ApiHelper::WRONG_TYPE);
 }
-if ($result -> count() < 1){
+if ($count < 1){
   if ($type == 'room')
     ApiHelper::fail(get_vocab($type . "_not_exist"), ApiHelper::ROOM_NOT_EXIST);
   else if ($type == 'area')
     ApiHelper::fail(get_vocab($type . "_not_exist"), ApiHelper::AREA_NOT_EXIST);
 }
 
-ApiHelper::success($result -> all_rows_keyed());
+ApiHelper::success($back);
