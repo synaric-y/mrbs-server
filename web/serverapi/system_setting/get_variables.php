34aad1a621605ca284a8cb5e829fb49670da9247
synaric.yong <synaric.yong@bcc.global>
2024-11-14 09:41:46 +0800

    细分get_variables的权限校验，区分必须登录参数和非必须参数

--- web/serverapi/system_setting/get_variables.php
+++ web/serverapi/system_setting/get_variables.php
@@ -9,10 +9,10 @@ namespace MRBS;
  * get variables when the variables be set 1
  */
 
-if (!checkAuth()){
-  setcookie("session_id", "", time() - 3600, "/web/");
-  ApiHelper::fail(get_vocab("please_login"), ApiHelper::PLEASE_LOGIN);
-}
+//if (!checkAuth()){
+//  setcookie("session_id", "", time() - 3600, "/web/");
+//  ApiHelper::fail(get_vocab("please_login"), ApiHelper::PLEASE_LOGIN);
+//}
 
 $vars = array(
   "use_wxwork",
@@ -47,17 +47,58 @@ $vars = array(
   "theme_type"
 );
 
+$must_login_vars = array(
+  "use_wxwork",
+  "use_exchange",
+  "Exchange_server",
+  "corpid",
+  "secret",
+  "agentid",
+//  "call_back_domain",
+  "redis_host",
+  "redis_post",
+  "redis_password",
+  "AD_server",
+  "AD_post",
+  "AD_base_dn",
+  "AD_username",
+  "AD_password",
+  "AD_interval_date",
+  "Exchange_sync_type",
+  "Exchange_sync_interval",
+//  "logo_dir",
+//  "app_logo_dir",
+  "time_type",
+  "now_version",
+  "show_book",
+  "show_meeting_name",
+  "temporary_meeting",
+  "resolution",
+//  "company_name",
+//  "init_status",
+//  "server_address",
+  "theme_type"
+);
+
 
 $result = db() -> query("SELECT * FROM " . _tbl("system_variable")) -> next_row_keyed();
-
+$must_login = false;
 foreach ($vars as $var) {
   if (empty($_POST[$var])){
     unset($result[$var]);
+  } else if (in_array($var, $must_login_vars)) {
+    $must_login = true;
   }
   if (getLevel($_SESSION['user']) < 2){
     if (!in_array($var, ['logo_dir', 'app_logo_dir', 'company_name', 'server_address'])) {
       unset($result[$var]);
     }
+  }
+}
+if ($must_login) {
+  if (!checkAuth()){
+    setcookie("session_id", "", time() - 3600, "/web/");
+    ApiHelper::fail(get_vocab("please_login"), ApiHelper::PLEASE_LOGIN);
   }
 }
 
