e2b795376bf1cdd35aad77c0e782ba66df53ab8e
synaric.yong <synaric.yong@bcc.global>
2024-11-18 10:24:56 +0800

    同版本覆盖

--- web/serverapi/system_setting/update_new_version.php
+++ web/serverapi/system_setting/update_new_version.php
@@ -21,17 +21,17 @@ if (getLevel($_SESSION['user']) < 2) {
 }
 
 $version = $_REQUEST['version'];
-$result = db() -> query1("SELECT COUNT(*) FROM " . _tbl("version") . " WHERE version = ?", array($version));
-if ($result > 0){
+$result = db()->query1("SELECT COUNT(*) FROM " . _tbl("version") . " WHERE version = ?", array($version));
+if ($result > 0) {
   ApiHelper::fail(get_vocab("version_exists"), ApiHelper::VERSION_EXISTS);
 }
 
 $file = $_FILES['file'];
 
-if($file['error'] === UPLOAD_ERR_OK){
+if ($file['error'] === UPLOAD_ERR_OK) {
   $name = $file['name'];
   $fileInfo = pathinfo($name);
-  if (strtolower($fileInfo['extension']) !== 'zip'){
+  if (strtolower($fileInfo['extension']) !== 'zip') {
     ApiHelper::fail(get_vocab("unsupport_file_type"), ApiHelper::UNSUPPORT_FILE_TYPE);
   }
   $zip = new ZipArchive;
@@ -56,19 +56,20 @@ if($file['error'] === UPLOAD_ERR_OK){
     deleteDirectory($dir . "/" . $version);
   }
   $dir .= "/" . $version . ".zip";
-  if (move_uploaded_file($file['tmp_name'], $dir)){
+  if (move_uploaded_file($file['tmp_name'], $dir)) {
     $zip = new ZipArchive;
-    if($zip -> open($dir) === TRUE){
-      $zip -> extractTo(dirname($dir));
-      $zip -> close();
-    }else{
+    if ($zip->open($dir) === TRUE) {
+      $zip->extractTo(dirname($dir));
+      $zip->close();
+    } else {
       ApiHelper::fail(get_vocab("unable_to_open_zip"), ApiHelper::UNABLE_TO_OPEN_ZIP);
     }
-  }else{
+  } else {
     ApiHelper::fail(get_vocab("fail_to_upload"), ApiHelper::FAIL_TO_UPLOAD);
   }
 
-  db() -> command("INSERT IGNORE INTO " . _tbl("version") . "(version, publish_time) VALUES (?, ?)", array($version, time()));
+  db()->command("DELETE * FROM " . _tbl("version") . " WHERE version = ?",array($version));
+  db()->command("INSERT IGNORE INTO " . _tbl("version") . "(version, publish_time) VALUES (?, ?)", array($version, time()));
 
   ApiHelper::success(null);
 }
