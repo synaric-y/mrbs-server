4f908884dbc1e454445dbc5bee2c0e43e06b1ab6
synaric.yong <synaric.yong@bcc.global>
2024-11-25 14:12:54 +0800

    联调后端生成二维码内容接口

--- web/wxwork_login_url.php
+++ web/wxwork_login_url.php
@@ -13,8 +13,10 @@ $config = DBHelper::one(_tbl("system_variable"), "1=1");
 
 $corpid = $config['corpid'];
 $agentid = $config['agentid'];
+$room_id = $_POST['room_id'];
+$area_id = $_POST['area_id'];
 $call_back_domain = $config['server_address'];
 $call_back = urlencode($call_back_domain);
 
-echo "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$corpid}&redirect_uri={$call_back}%2Fapp%2Findex.html%23%2Fcb&response_type=code&scope=snsapi_privateinfo&agentid={$agentid}&state=#wechat_redirect";
+echo "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$corpid}&redirect_uri={$call_back}%2Fapp%2Findex.html%23%2Fpages%2Fcb%2Fcb?room_id={$room_id}%26area_id={$area_id}&response_type=code&scope=snsapi_privateinfo&agentid={$agentid}&state=#wechat_redirect";
 
