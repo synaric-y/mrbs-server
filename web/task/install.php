970296fde03ec0572d07f6c54aaf9930751aab03
synaric.yong <synaric.yong@bcc.global>
2024-12-05 17:52:58 +0800

    增加安装脚本提示

--- web/task/install.php
+++ web/task/install.php
@@ -6,6 +6,8 @@ namespace MRBS;
  * This is a setup script to insert init data into DB.
  */
 
+echo "install start", PHP_EOL;
+
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . "/defaultincludes.inc";
 require_once dirname(__DIR__) . "/functions_table.inc";
@@ -15,3 +17,5 @@ $pwd = password_hash('123456', PASSWORD_DEFAULT);
 
 db()->command("INSERT INTO mrbs_users (level, name, display_name, email, password_hash) values (2, 'admin', 'admin', '', '$pwd')");
 db()->command("UPDATE mrbs_system_variable SET default_password_hash = '$pwd'");
+
+echo "install completed!";
